10da8536e95d12d5a54fc604f5ee33584546d04f
Lacina Jakub <lacina.j@sosceskybrod.cz>
2023-06-07 14:15:02 +0200

    added nav

---
+++ php/nav/menu.php
@@ -0,0 +1,15 @@
+<div class="menu">
+    <img class="logo" src="page/img/logo.png" alt="logo">
+    <div class="container-box">
+
+        <a class="search-a" href=https://www.w3schools.com/css/css3_object-fit.asp>
+            <img src="page/img/search.png" width="35px">
+        </a>
+        <a class="panak-a" href=https://www.w3schools.com/css/css3_object-fit.asp>
+            <img src="page/img/panak.png" width="35px">
+        </a>
+        <a class="text-a" href=https://www.w3schools.com/css/css3_object-fit.asp>
+            Přidat inzerát
+        </a>
+    </div>
+</div>
