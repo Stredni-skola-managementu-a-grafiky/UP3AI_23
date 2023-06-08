527290d271f86de2809d9b3babf9ed1d34668be7
Aleš Skalický <skalicky.a@sosceskybrod.cz>
2023-06-08 13:15:41 +0200

    db log added

---
+++ php/db/database_log.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$servername = "ita03.vas-server.cz";
+
+$username = "kdestosehnal_cz";
+$password = "tOVi0eApgizmcyUO";
+
+$db_name = "kdestosehnal_cz";
+
+$database_log = new mysqli($servername,$username,$password,$db_name);
+?>
